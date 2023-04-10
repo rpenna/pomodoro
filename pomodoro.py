4ee544bb1ef4cd9d3494de5c34139bcd2c441a56
Rafael Penna <rafaelgpenna@gmail.com>
2023-04-10 18:12:30 -0300

    feat: create control flow

---
+++ pomodoro.py
@@ -0,0 +1,113 @@
+from argparse import Namespace, ArgumentParser
+from datetime import datetime
+
+
+class StopPomodoroException(Exception):
+    ...
+
+
+def get_args() -> ArgumentParser:
+    """Read arguments and define pomodoro settings.
+
+    Returns:
+        ArgumentParser: pomodoro settings
+    """
+    ...
+
+
+def wait_for_permission_to_run() -> None:
+    """Print possible commands and wait for user's input.
+
+    Raises:
+        StopPomodoroException: raised when user wants to end pomodoro
+        execution.
+    """
+    raise StopPomodoroException
+
+
+def run_focus_time(pomodoro_time: int) -> datetime:
+    """Run focus time (pomodoro), showing progression bar and pomodoro stats.
+
+    Args:
+        pomodoro_time (int): Pomodoro duration (minutes)
+
+    Returns:
+        datetime: pomodoro start time
+    """
+    ...
+
+
+def record_progress(finished_pomodoros: int, start_time: datetime) -> None:
+    """Save pomodoro stats
+
+    Args:
+        finished_pomodoros (int): number of pomodoros finished
+        start_time (datetime): start time of last pomodoro runned
+    """
+    ...
+
+
+def get_break_duration(
+    finished_pomodoros: int,
+    pomodoro_settings: dict
+) -> int:
+    """Calculates next break duration
+
+    Args:
+        finished_pomodoros (int): number of finished pomodoros
+        pomodoro_settings (dict): pomodoro settings (needs buffer length and
+        breaks duration)
+
+    Returns:
+        int: next break duration (minutes)
+    """
+    ...
+
+
+def run_break(minutes: int) -> None:
+    """Run break, showing progress bar and pomodoro stats
+
+    Args:
+        minutes (int): break length in minutes
+    """
+    ...
+
+
+def show_progress(pomodoro_settings: dict, finished_pomodoros: int) -> None:
+    """Print pomodoro stats
+
+    Args:
+        pomodoro_settings (dict): Pomodoro settings (use all of them)
+        finished_pomodoros (int): number of finished pomodoros
+    """
+    ...
+
+
+def start_pomodoro_counter(pomodoro_settings: dict) -> None:
+    finished_pomodoros = 0
+    quit_pomodoro = False
+    try:
+        while not quit_pomodoro:
+            wait_for_permission_to_run()
+            start_time = run_focus_time(pomodoro_settings['pomodoro_time'])
+            finished_pomodoros += 1
+            break_duration = get_break_duration(
+                finished_pomodoros,
+                pomodoro_settings,
+            )
+            record_progress(finished_pomodoros, start_time)
+            wait_for_permission_to_run()
+            run_break(break_duration)
+    except (StopPomodoroException, KeyboardInterrupt):
+        show_progress(pomodoro_settings)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    pomodoro_settings = {
+        'pomodoro_time': args.pt,
+        'short_break_time': args.sbt,
+        'long_break_time': args.lbt,
+        'buffer_length': args.bl
+    }
+    start_pomodoro_counter(pomodoro_settings)

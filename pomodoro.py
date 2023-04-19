0f0a739d44a08a3222405d11d00fea13394573be
Rafael Penna <rafaelgpenna@gmail.com>
2023-04-19 15:59:45 -0300

    feat: add progress bar to pomodoro execution

--- pomodoro.py
+++ pomodoro.py
@@ -5,13 +5,40 @@ from tqdm import tqdm
 from exceptions.stop_pomodoro_exception import StopPomodoroException
 
 
-def get_args() -> ArgumentParser:
+def get_args() -> Namespace:
     """Read arguments and define pomodoro settings.
 
     Returns:
         ArgumentParser: pomodoro settings
     """
-    ...
+    parser = ArgumentParser(
+        description='Command line pomodoro execution'
+    )
+    parser.add_argument(
+        '-pt',
+        default=25,
+        type=int,
+        help='Pomodoro time: focus time duration (in minutes)'
+    )
+    parser.add_argument(
+        '-sbt',
+        default=5,
+        type=int,
+        help='Short break time: short break durantion (in minutes)'
+    )
+    parser.add_argument(
+        '-lbt',
+        default=15,
+        type=int,
+        help='Long break time: long break duration (in minutes)'
+    )
+    parser.add_argument(
+        '-bl',
+        default=4,
+        type=int,
+        help='Buffer length: number of pomodoros cycles until long break'
+    )
+    return parser.parse_args()
 
 
 def wait_for_permission_to_run() -> None:
@@ -31,22 +58,31 @@ def wait_for_permission_to_run() -> None:
         raise StopPomodoroException
 
 
-def sleep_with_progress_bar(sleep_time: float) -> None:
-    for _ in tqdm(range(100)):
-        time.sleep(sleep_time)
+def sleep_with_progress_bar(sleep_time: float, description: str = '') -> None:
+    """Show progress bar to user
+
+    Args:
+        sleep_time (float): progress duration in seconds
+        description (str, optional): Description of what is running. Defaults to ''.
+    """
+    minutes = str(sleep_time // 60).zfill(2)
+    seconds = str(sleep_time % 60).zfill(2)
+    bar_format = '{l_bar}{bar:30}| [{elapsed}/%s:%s]' % (minutes, seconds)
+    for _ in tqdm(range(sleep_time), desc=description, bar_format=bar_format):
+        time.sleep(1)
 
 
 def run_focus_time(pomodoro_time: int) -> datetime:
     """Run focus time (pomodoro), showing progression bar and pomodoro stats.
 
     Args:
-        pomodoro_time (int): Pomodoro duration (seconds)
+        pomodoro_time (int): Pomodoro duration (minutes)
 
     Returns:
         datetime: pomodoro start time
     """
     print('Running pomodoro...')
-    one_percent_interval = pomodoro_time / 100
+    one_percent_interval = (pomodoro_time * 60)
     start_time = datetime.now()
     sleep_with_progress_bar(one_percent_interval)
     print('Pomodoro finished')
@@ -84,10 +120,10 @@ def run_break(break_time: int) -> None:
     """Run break, showing progress bar and pomodoro stats
 
     Args:
-        break_time (int): break length in seconds
+        break_time (int): break duration in minutes
     """
     print('Running break...')
-    one_percent_interval = break_time / 100
+    one_percent_interval = (break_time * 60)
     start_time = datetime.now()
     sleep_with_progress_bar(one_percent_interval)
     print('Break finished')
@@ -120,7 +156,7 @@ def start_pomodoro_counter(pomodoro_settings: dict) -> None:
             wait_for_permission_to_run()
             run_break(break_duration)
     except (StopPomodoroException, KeyboardInterrupt):
-        show_progress(pomodoro_settings)
+        show_progress(pomodoro_settings, finished_pomodoros)
 
 
 if __name__ == '__main__':
